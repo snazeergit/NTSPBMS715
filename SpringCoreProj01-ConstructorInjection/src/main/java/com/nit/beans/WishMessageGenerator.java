8dcda9738bfab3985e4598b6025b442e2ae29bcd
Nazeer <nazeersd.java@gmail.com>
2022-04-05 09:40:36 +0530

    First commit

---
+++ SpringCoreProj01-ConstructorInjection/src/main/java/com/nit/beans/WishMessageGenerator.java
@@ -0,0 +1,27 @@
+package com.nit.beans;
+
+import java.util.Date;
+
+public class WishMessageGenerator {
+
+	private Date date;
+
+	public WishMessageGenerator(Date date) {
+		this.date=date;
+		System.out.println("WishMessageGenerator.WishMessageGenerator(Date)");
+	}
+	
+	public String generateWishMessage() {
+		int hours = date.getHours();
+		String msg = null;
+		if (hours<12 && hours>6) {
+			return msg="Good Morning";
+		}else if (hours>12 && hours<18) {
+			return msg="Good Afternoon";
+		}else if (hours>18) {
+			return msg="Good Evening";
+		}
+		return msg; 
+	}
+
+}
---
+++ SpringCoreProj01-ConstructorInjection/src/main/java/com/nit/test/WishClientApplication.java
@@ -0,0 +1,20 @@
+package com.nit.test;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+import com.nit.beans.WishMessageGenerator;
+
+@SuppressWarnings("deprecation")
+public class WishClientApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		FileSystemResource resource = new FileSystemResource("src/main/java/com/nit/cfgs/applicationContext.xml");
+		XmlBeanFactory beanFactory = new XmlBeanFactory(resource);
+		Object bean = beanFactory.getBean("wmg");
+		WishMessageGenerator generator = (WishMessageGenerator) bean;
+		System.out.println(generator.generateWishMessage()+" Nazeer!");
+	}
+}
