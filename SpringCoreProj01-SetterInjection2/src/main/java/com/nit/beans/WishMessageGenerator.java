b095abad446d9358826e5fea6ffc177ee69629b1
Nazeer <nazeersd.java@gmail.com>
2022-04-05 18:53:52 +0530

    ClassPathResource example

---
+++ SpringCoreProj01-SetterInjection2/src/main/java/com/nit/beans/WishMessageGenerator.java
@@ -0,0 +1,31 @@
+package com.nit.beans;
+
+import java.util.Date;
+
+public class WishMessageGenerator {
+
+	private Date date;
+
+	public WishMessageGenerator() {
+		System.out.println("WishMessageGenerator.WishMessageGenerator()");
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+		System.out.println("WishMessageGenerator.setDate()");
+	}
+	
+	public String generateWishMessage() {
+		int hours = date.getHours();
+		String msg = null;
+		if (hours<12 && hours>6) {
+			return msg="Good Morning";
+		}else if (hours>12 && hours<18) {
+			return msg="Good Afternoon";
+		}else if (hours>18) {
+			return msg="Good Evening";
+		}
+		return msg; 
+	}
+
+}
---
+++ SpringCoreProj01-SetterInjection2/src/main/java/com/nit/test/WishClientApplication.java
@@ -0,0 +1,21 @@
+package com.nit.test;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
+import com.nit.beans.WishMessageGenerator;
+
+public class WishClientApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//FileSystemResource resource = new FileSystemResource("src/main/java/com/nit/cfgs/applicationContext.xml");
+		ClassPathResource resource=new ClassPathResource("com/nit/cfgs/applicationContext.xml");
+		XmlBeanFactory beanFactory = new XmlBeanFactory(resource);
+		Object bean = beanFactory.getBean("wmg");
+		WishMessageGenerator generator = (WishMessageGenerator) bean;
+		System.out.println(generator.generateWishMessage()+" Nazeer!");
+	}
+}
