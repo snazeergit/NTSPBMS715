7e7a9f718ea418130c94f99b358058d22f3cccf1
Nazeer <nazeersd.java@gmail.com>
2022-04-16 13:59:20 +0530

    DefaultListableBeanFactory

---
+++ SpringCore01-DefaultListableBeanFactory/src/main/java/nit/java/test/CalculatorClient.java
@@ -0,0 +1,19 @@
+package nit.java.test;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import nit.java.beans.Calculator;
+
+public class CalculatorClient {
+	public static void main(String[] args) {
+		ClassPathResource classpath = new ClassPathResource("nit/java/cfgs/applicationContext.xml");
+		XmlBeanFactory factory = new XmlBeanFactory(classpath);
+		Object bean = factory.getBean("calc");
+		Calculator cal = (Calculator) bean;
+		cal.addition(20, 50);
+
+	}
+}
