3d6388936e8e215a6d1f1ba5ea506e0ed7a8e185
Nazeer <nazeersd.java@gmail.com>
2022-05-10 20:37:25 +0530

    ConstructorInjection-Research

--- SpringCore01-DependencyInjection2/src/main/java/com/nit/beans/Calculator.java
+++ SpringCore01-DependencyInjection2/src/main/java/com/nit/beans/Calculator.java
@@ -9,19 +9,27 @@ public class Calculator {
 		System.out.println("Calculator.Calculator()");
 	}
 
-	/*
-	 * public Calculator(String c, int a) { this.c = c; this.a = a;
-	 * System.out.println("Calculator.Calculator(String c, int a)"); }
-	 */
+	public Calculator(String c, int a) {
+		this.c = c;
+		this.a = a;
+		System.out.println("Calculator.Calculator(String c, int a)");
+	}
 
 	public Calculator(int a, String c) {
 		this.a = a;
 		this.c = c;
 		System.out.println("Calculator.Calculator(int a, String c)");
 	}
-	
+
 	public void printProperties() {
-		System.out.println("a: "+a+" c= "+c);
+		System.out.println("a: " + a + " c= " + c);
 	}
 
 }
+
+/*When a Spring bean is configured for Constructor injection for its bean properties and multiple constructors are placed to support this injection 
+then IOC container prefer to choose the Constructor with the parameter order same as the spring bean configuration file. 
+That means this Spring bean class is having Calculator(int a , String c) and Calculator(String c, int a) to inject values to a and c properties now
+Constructor Calculator(int a, String c) will be used for injection as it follows order of arguments however if there is only one constructor with 
+different order of parameters than the spring bean config file then it will use the one that is available.
+*/
