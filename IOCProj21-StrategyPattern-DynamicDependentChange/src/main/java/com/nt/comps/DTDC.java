ed6f31fd2d1d92cc4f3db20171ac1baaa5c5b1bf
Nazeer <nazeersd.java@gmail.com>
2022-05-11 23:36:16 +0530

    StrategyPattern-DynamicDependentChange

---
+++ IOCProj21-StrategyPattern-DynamicDependentChange/src/main/java/com/nt/comps/DTDC.java
@@ -0,0 +1,18 @@
+package com.nt.comps;
+
+import org.springframework.stereotype.Component;
+
+@Component("dtdc")
+public final class DTDC implements Courier {
+
+	public DTDC() {
+		System.out.println("DTDC:: Zero-param Constructor");
+	}
+
+	@Override
+	public String deliver(int orderId) {
+		// TODO Auto-generated method stub
+		return orderId + " order has been assigned to DTDC for Delivery";
+	}
+
+}
