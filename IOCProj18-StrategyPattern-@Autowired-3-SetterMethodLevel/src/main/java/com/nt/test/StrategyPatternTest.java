e294c6ecca4b2f348e2059b4c6e3c9e9d525c65f
Nazeer <nazeersd.java@gmail.com>
2022-05-19 22:55:21 +0530

    StrategyPattern-@Autowired-3-SetterMethodLevel

---
+++ IOCProj18-StrategyPattern-@Autowired-3-SetterMethodLevel/src/main/java/com/nt/test/StrategyPatternTest.java
@@ -0,0 +1,19 @@
+package com.nt.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.comps.Flipkart;
+
+public class StrategyPatternTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/nt/config/applicationContext.xml");
+		Flipkart flipkart2 = context.getBean("flipkart", Flipkart.class);
+		String resultMsg = flipkart2.shopping(new String[] { "candles", "flowers", "cake", "wine" },
+				new double[] { 1000.0, 2000.0, 3000.0, 7000.0 });
+		System.out.println(resultMsg);
+		context.close();
+	}
+}
