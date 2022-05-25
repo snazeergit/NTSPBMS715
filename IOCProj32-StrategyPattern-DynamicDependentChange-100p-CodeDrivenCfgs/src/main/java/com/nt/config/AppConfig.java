73dc807f3758e24dd9aebfd84e9a1f62b3410955
Nazeer <nazeersd.java@gmail.com>
2022-05-25 20:51:59 +0530

    StrategyPattern-DynamicDependentChange-100p-CodeDrivenCfgs

---
+++ IOCProj32-StrategyPattern-DynamicDependentChange-100p-CodeDrivenCfgs/src/main/java/com/nt/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.nt.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ComponentScan(basePackages = "com.nt")
+@ImportResource(locations = "com/nt/cfgs/applicationContext.xml")
+public class AppConfig {
+
+	public AppConfig() {
+		System.out.println("AppConfig:: 0-param Constructure");
+	}
+}
