0e00c60a77193c9dae0f3484e7b609c24cbb3391
snazeer <snazeer@snazeer-z02>
2023-01-26 13:39:57 +0530

    Setter N Constructor Injection updated.

--- SpringCoreProj01-SetterAndConstructorInjection/src/main/java/com/nt/beans/WishMessageGenerator.java
+++ SpringCoreProj01-SetterAndConstructorInjection/src/main/java/com/nt/beans/WishMessageGenerator.java
@@ -6,24 +6,25 @@ public class WishMessageGenerator {
 	private Date date;
 
 	public WishMessageGenerator(Date date) {
-			this.date = date;
-			System.out.println("WishMessageGenerator.WishMessageGenerator(Date)"+date);
+		this.date = date;
+		System.out.println("Constructor Injection Performed:: " + date);
 	}
-	
+
 	public void setDate(Date date) {
-		this.date=date;
-		System.out.println("WishMessageGenerator.setDate(Date)"+date);
+		this.date = date;
+		System.out.println("Setter Injection Performed:: " + date);
 	}
-	
+
 	public String generateWishMessage() {
+		System.out.println("Finally date property has been injected with value:: " + date);//setter injection overrides the constructor injection date property value.
 		@SuppressWarnings("deprecation")
 		int hours = date.getHours();
-		if(hours<12&& hours >6)
+		if (hours < 12 && hours > 6)
 			return "Good Morning";
-		else if (hours>12&& hours<18)
+		else if (hours > 12 && hours < 18)
 			return "Good Afternoon";
-		else if (hours>18&& hours<20) 
-				return "Good Evening";
+		else if (hours > 18 && hours < 20)
+			return "Good Evening";
 		else
 			return "Good Night";
 	}
